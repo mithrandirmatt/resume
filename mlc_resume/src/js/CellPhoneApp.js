5982eed93c60a8012fe073a665fdc8886b164091
Matt <mithrandirmatt@yahoo.com>
2019-12-31 15:30:02 -0500

    Updated stuff...

---
+++ mlc_resume/src/js/CellPhoneApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+import '../css/CellPhoneApp.css';
+
+
+export default class CellPhoneApp extends Component {
+
+    render() {
+        return (
+            <div id="CellPhoneApp" className="CellPhoneApp">
+                <div id="CellPhoneAppPhoneIcon" className="CellPhoneAppPhoneIcon"></div>
+            </div>
+        );
+    }
+}
--- mlc_resume/src/js/ContentHome.js
+++ mlc_resume/src/js/ContentHome.js
@@ -1,41 +1,55 @@
 import React, { Component } from 'react';
 
 import DynamicContentWidget from './DynamicContentWidget'
+import CellPhoneApp from './CellPhoneApp'
+import Log from './Log'
 
 import '../css/ContentHome.css';
 
 const creatingProps = {
     id: "ContentHomeCreatingContext",
     label: "Creating",
-    text: "Producing unique designs.",
-    color: "red",
+    text: "Producing unique designs."
 }
 
 const collaboratingProps = {
     id: "ContentHomeCollaboratingContext",
     label: "Collaborating",
-    text: "Working with diverse teams to deliver.",
-    color: "red",
+    text: "Working with diverse teams to deliver."
 }
 
 const innovatingProps = {
     id: "ContentHomeInnovatingContext",
     label: "Innovating",
-    text: "Spicing up the recipes.",
-    color: "red",
+    text: "Spicing up the recipes."
 }
 
 export default class ContentHome extends Component {
+
+    componentDidMount() {
+        window.onscroll = function(e){
+            var scroll = document.body.scrollTop;
+            console.log(  document, e, e.target.scrollingElement.scrollTop );
+            Log.info(" scrollTop == " + scroll, 'App Component' );
+
+            if( scroll > 200 && scroll < 1000 )
+            {
+                var id = document.getElementById("ContentHomeFirstSectionCommon");
+                id.classList.add( "ScrollUpShow" );
+            }
+        };
+    }
 
     render() {
         return (
             <div id="ContentHome" className="ContentHome">
 
-                <div className="ContentHomeFirstSectionCommon">
+                <div id="ContentHomeFirstSectionCommon" className="ContentHomeFirstSectionCommon">
                     <div className="ContentHomeFirstSectionWrapper">
                         <DynamicContentWidget context={creatingProps} id="ContentHomeCreating" />
                         <DynamicContentWidget context={collaboratingProps} id="ContentHomeCollaborating" />
                         <DynamicContentWidget context={innovatingProps} id="ContentHomeInnovation" />
+                        <CellPhoneApp />
                     </div>
                 </div>
             </div>
